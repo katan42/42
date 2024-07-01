7d994a1294a0a71e1c8dedbd51db75243999ce73
Ka Shin Tan <katan@c2r5s2.42singapore.sg>
2024-07-01 18:30:30 +0800

    modified ft_print

--- 02_ft_printf/ft_print.c
+++ 02_ft_printf/ft_print.c
@@ -6,10 +6,44 @@
 /*   By: katan <katan@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 16:47:57 by katan             #+#    #+#             */
-/*   Updated: 2024/06/29 16:48:38 by katan            ###   ########.fr       */
+/*   Updated: 2024/07/01 18:29:16 by katan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static void ft_printf_check(char s, va_list *args, int *count)
+{
+	if (s == 'c')
+		ft_printchar(va_arg(*args, int), count);
+
+
+
 int	ft_printf(const char *s, ...)
+{
+	va_list args;
+	int		i;
+
+	if(!s)
+		return (0);
+
+	i = 0;
+	va_start(args, s);
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '%')
+		{
+			i++;
+			ft_printf_check(s[i], &args, &count);
+		}
+		else
+		{
+			ft_printchar((char)s[i]. &count);
+		}
+		i++;
+	}
+
+	va_end(args);
+	return (count);
+}
