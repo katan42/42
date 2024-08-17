3acd5aa0183619c22bfca1c002c77f43fbfd47ba
Ka Shin Tan <katan@c2r5s2.42singapore.sg>
2024-08-17 16:02:34 +0800

    added the helper fns

--- 03_get_next_line/get_next_line_utils.c
+++ 03_get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: katan <katan@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 14:04:13 by katan             #+#    #+#             */
-/*   Updated: 2024/08/16 18:51:56 by katan            ###   ########.fr       */
+/*   Updated: 2024/08/17 16:00:32 by katan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,18 @@ char	*ft_strchr(const char *s, int i)
 		}
 	}
 	return (NULL);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
 char	*ft_strdup(const char *s)
@@ -48,3 +60,69 @@ char	*ft_strdup(const char *s)
 	str[j] = '\0';
 	return (str);
 }
+
+size_t ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (src[i] != '\0')
+		i++;
+	if (size == 0)
+		return (i);
+	j = 0;
+	while (j < size - 1 && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	if (size > 0)
+		dest[j] = '\0';
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	slen;
+	size_t	sublen;
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	slen = ft_strlen(s);
+	if (start >= slen)
+		return (ft_strdup(""));
+	sublen = slen - start;
+	if (len > sublen)
+		len = sublen;
+	str = malloc(len + 1);
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, S + start, len + 1);
+	return (str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	str = (char *)malloc(sizeof(char) *(ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);
