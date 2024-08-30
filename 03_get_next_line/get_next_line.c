8a20562618449def33aebb64a5c1261b3dbed71a
Ka Shin Tan <katan@c2r6s1.42singapore.sg>
2024-08-30 19:32:30 +0800

    edited

--- 03_get_next_line/get_next_line.c
+++ 03_get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: katan <katan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 14:00:52 by katan             #+#    #+#             */
-/*   Updated: 2024/08/30 17:56:24 by katan            ###   ########.fr       */
+/*   Updated: 2024/08/30 19:28:12 by katan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,19 @@
 
 char	*get_exact_line(char *line)
 {
+	char	*exact_line;
+	int		i;
+
+	i = 0;
+	while (line[i] && line[i] != '\n')
+		i++;
+	if (!line[i])
+		i--;
+	exact_line = ft_substr(line, 0, i + 2);
+	free(line);
+	if (!exact_line)
+		return (NULL);
+	return (exact_line);
 }
 
 char	*ft_strchr(const char *s, int i)
@@ -31,7 +44,7 @@ char	*ft_strchr(const char *s, int i)
 	return (NULL);
 }
 
-static char	*fill_line_buffer(int fd, char *left, char *buffer)
+static char	*fill_line_buffer(int fd, char **left, char *buffer)
 {
 	ssize_t	buff_read;
 	char	*temp;
@@ -46,21 +59,21 @@ static char	*fill_line_buffer(int fd, char *left, char *buffer)
 		else if (buff_read == 0)
 			break ;
 		buffer[buff_read] = '\0';
-		if (!left)
-			left = ft_strdup("");
-		if (!left)
+		if (!*left)
+			*left = ft_strdup("");
+		if (!*left)
 			return (NULL);
-		temp = left;
-		left = ft_strjoin(temp, buffer);
+		temp = *left;
+		*left = ft_strjoin(temp, buffer);
 		free(temp);
 		temp = NULL;
-		if(!left)
+		if(!*left)
 			return (NULL);
-		check_nl = ft_strchr(left, '\n');
+		check_nl = ft_strchr(*left, '\n');
 		if (check_nl)
 			break;
 	}
-	return (left);
+	return (*left);
 }
 
 static char	*set_line(char *line_buffer)
@@ -72,14 +85,14 @@ static char	*set_line(char *line_buffer)
 	while (line_buffer[i] != '\n' && line_buffer[i] != '\0')
 		i++;
 	if (line_buffer[i] == 0)
-		return (NULL);
+		return ((void *)1);
 	left = ft_substr(line_buffer, i + 1, ft_strlen(line_buffer) - i);
 	if (!left)
 			return (NULL);
 	if (*left == 0)
 	{
 		free(left);
-		left = NULL;
+		left = (void *)1;
 	}
 	line_buffer[i + 1] = '\0';
 	return (left);
@@ -96,12 +109,18 @@ char	*get_next_line(int fd)
 		return (free(left), left = NULL, free(buffer), NULL);
 	if (!buffer)
 		return (free(left), left = NULL, NULL);
-	line = fill_line_buffer(fd, left, buffer);
+	line = fill_line_buffer(fd, &left, buffer);
 	free(buffer);
 	buffer = NULL;
 	if (!line)
 		return (free(left), left = NULL, NULL);
 	left = set_line(line);
+	if (!left)
+		return (free(line), NULL);
+	else if (left == (void *)1)
+		left = NULL;
 	line = get_exact_line(line);
+	if (!line)
+		return (free(left), left = NULL, NULL);
 	return (line);
 }
