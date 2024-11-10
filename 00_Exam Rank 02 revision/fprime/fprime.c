37bd54f13e32708a9ae4114089e40f3bb5476190
Ka Shin Tan <katan@c2r5s2.42singapore.sg>
2024-11-10 20:51:18 +0800

    revision

---
+++ 00_Exam Rank 02 revision/fprime/fprime.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fprime.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: katan <katan@student.42singapore.sg>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/10 20:24:30 by katan             #+#    #+#             */
+/*   Updated: 2024/11/10 20:38:18 by katan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+	prime = i / i 
+
+	int prime;
+
+	if number % prime == 0
+		number = number/prime
---
+++ 00_Exam Rank 02 revision/ft_atoi/ft_atoi.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: katan <katan@student.42singapore.sg>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/10 17:51:21 by katan             #+#    #+#             */
+/*   Updated: 2024/11/10 19:23:39 by katan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	result = 0;
+	sign = 1;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (sign * result);
+}
+/*
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		printf("%d\n", ft_atoi(argv[1]));
+	}
+}
+*/
