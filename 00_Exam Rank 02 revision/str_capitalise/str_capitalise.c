c6cec99c829949e54b9b404bb5f684232a28939d
Ka Shin Tan <katan@c2r5s2.42singapore.sg>
2024-11-15 20:24:27 +0800

    str_capitalise

--- 00_Exam Rank 02 revision/str_capitalise/str_capitalise.c
+++ 00_Exam Rank 02 revision/str_capitalise/str_capitalise.c
@@ -1,23 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fprime.c                                           :+:      :+:    :+:   */
+/*   str_capitalise.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: katan <katan@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/10 20:24:30 by katan             #+#    #+#             */
-/*   Updated: 2024/11/10 20:38:18 by katan            ###   ########.fr       */
+/*   Created: 2024/11/15 15:22:23 by katan             #+#    #+#             */
+/*   Updated: 2024/11/15 15:57:21 by katan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <unistd.h>
 #include <stdio.h>
 
 int main(int argc, char **argv)
 {
-	int	i;
-	int prime;
+	int i;
+	int j;
+	int count;
 
-	number =c		
-	if (number % prime == 0)
-		number = number/prime;
+	i = 0;
+	j = 1;
+	if(argc == 1)
+		write(1, "\n", 1);
+	count = argc;
+	while (count>1)
+	{
+		while (argv[j][i] != '\0')
+		{
+			if (argv[j][i] >= 'a' && argv[j][i] <= 'z')
+			{
+				if (i == 0 || argv[j][i - 1] == 32 || argv[j][i - 1] == 9)
+				{
+					argv[j][i] -= 32;
+				}
+			}
+			else if (argv[j][i] >= 'A' && argv[j][i] <= 'Z')
+			{
+				argv[j][i] += 32;
+			}
+			write(1, &argv[j][i], 1);
+			i++;
+		}
+		j++;
+		count--;
+		}
+		write(1, "\n",1);
+
+	}
